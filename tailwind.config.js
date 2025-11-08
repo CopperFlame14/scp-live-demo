45ef0efde11b86eff9412dd144ac82e567167259
krishni <krishnimr14@gmail.com>
2025-11-08 08:36:18 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {
@@ -13,18 +13,18 @@ export default {
         primary: {
           100: "#a5f3fc",
           500: "#06b6d4",
-          700: "#0e7490",
+          700: "#0e7490"
         },
         dark: {
           900: "#0f172a",
           800: "#1e293b",
-          700: "#334155",
-        },
+          700: "#334155"
+        }
       },
       backgroundImage: {
-        "gradient-dark": "linear-gradient(to bottom right, #0f172a, #1e293b, #0f172a)",
-      },
-    },
+        "gradient-dark": "linear-gradient(to bottom right, #0f172a, #1e293b, #0f172a)"
+      }
+    }
   },
   plugins: [],
 };
