c50d31d790b1e61395683955b9772c42c92b4d1a
krishni <krishnimr14@gmail.com>
2025-11-08 09:32:08 +0530

    Delete postcss.config.js

--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-// PostCSS configuration for Tailwind v4 (Render-compatible)
-import tailwindcss from "@tailwindcss/postcss";
-import autoprefixer from "autoprefixer";
-
-export default {
-  plugins: [tailwindcss, autoprefixer],
-};
